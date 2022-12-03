b965370d7e111513bba261fc3e0875ef93f6239b
Basant Khati <mightyblue01@users.noreply.github.com>
2022-12-04 10:34:58 +1100

    Update verify_etl_app.sh

--- verify_etl_app.sh
+++ verify_etl_app.sh
@@ -3,7 +3,7 @@ docker exec --interactive kafka-etl-app_broker_1  kafka-console-producer --boots
 echo "published messages to input_topic"
 
 # messages published in input_topic will be processed by kafka etl app and published to output_topic
-# we now read those processed messages from output_topic and dump in a file processed_messages.jsonl
+# we now read those processed messages from output_topic and dump in a file processed_messages.txt
 docker exec  --interactive kafka-etl-app_broker_1 kafka-console-consumer --bootstrap-server broker:9092 --topic output_topic --from-beginning --group groupa --timeout-ms 5000 > processed_messages.txt
 
 echo "captured processed messages in processed_messages.txt file"
