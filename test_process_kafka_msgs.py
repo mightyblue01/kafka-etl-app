f40f7f3c1131dde0bcf4d237d53d583a1a0a38fb
Basant Khati <basant.khati@gmail.com>
2022-12-04 16:42:40 +1100

    updated UTs

--- test_process_kafka_msgs.py
+++ test_process_kafka_msgs.py
@@ -23,12 +23,12 @@ def test_total_messages_count(sample_json_data):
 
 
 def test_json_message_loaded_as_expected(sample_json_data):
-    assert "{'myKey': 11, 'myTimestamp': '2022-03-01T09:11:04+01:00'}" \
+    assert "{'myKey': 1, 'myTimestamp': '2022-03-01T09:11:04+01:00'}" \
            == str(sample_json_data[0])
 
 
 def test_process_message_method_for_valid_message(sample_json_data):
-    expected_msg = {"myKey": 12, "myTimestamp": "2022-03-01T08:12:08+0000",
+    expected_msg = {"myKey": 3, "myTimestamp": "2022-03-01T08:12:08+0000",
                         "key2": "2022-03-01"}
     assert expected_msg == process_message(sample_json_data[2])
 
